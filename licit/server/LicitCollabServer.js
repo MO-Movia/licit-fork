f70bf49adf95955568a09d9a54b24ca9bdee45f5
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2020-07-14 03:33:19 +0530

    Fixes: 1. Saving file not happening. 2. Not returning image on GET call.

--- licit/server/LicitCollabServer.js
+++ licit/server/LicitCollabServer.js
@@ -1,5 +1,4 @@
 // @flow
-
 import url from 'url';
 // [FS] IRAD-1004 2020-07-07
 const formidable = require('formidable');
@@ -7,6 +6,8 @@ const formidable = require('formidable');
 // Upgrade outdated packages.
 const uuidv4 = require('uuid').v4;
 const mv = require('mv');
+const path = require('path');
+const fs = require('fs');
 
 import LicitCollabController from './LicitCollabController';
 
@@ -19,76 +20,133 @@ class LicitCollabServer {
 
   handleRequest = (request: any, response: any): void => {
     const parsed: Object = url.parse(request.url, true);
-    const path = parsed.pathname || '';
+    const reqPath = parsed.pathname || '';
     const method = request.method.toUpperCase();
-    request.path = path;
-    log({ method, path });
+    let skip = false;
+    request.path = reqPath;
+    //log({ method, reqPath });
+    if (method === 'OPTIONS') {
       // For X-Domain Preflught Request
       // https://gist.github.com/nilcolor/816580
-    const headers = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': ';POST, GET, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Credential': false,
-      'Access-Control-Max-Age': 86400, // 24hrs
-      'Access-Control-Allow-Headers':
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept',
-    };
-    response.writeHead(200, headers);
-    if (method === 'OPTIONS') {
+      const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': ';POST, GET, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Credential': false,
+        'Access-Control-Max-Age': 86400, // 24hrs
+        'Access-Control-Allow-Headers':
+          'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept',
+      };
+      response.writeHead(200, headers);
       response.end();
     } else if (method === 'POST') {
-      let body = '';
-      request.on('data', function (chunk) {
-        body += chunk.toString();
-      });
       // [FS] IRAD-1004 2020-07-07
       // Image upload
       const se = /^\/saveimage/;
-      if (se.test(path)) {
-
+      if (se.test(reqPath)) {
+        skip = true;
         const form = new formidable.IncomingForm();
         const fileid = uuidv4();
         const query = parsed.query;
         log(query);
         const filename = fileid + '_' + query['fn'];
+        const host = request.headers['host'];
+        const proto = request.connection.encrypted ? 'https' : 'http';
+        log(proto);
         form.parse(request, function (err, fields, blob) {
           const oldpath = blob.file.path;
-          let respdata;
-          // const newpath = __dirname + '/images/' + filename;
-          const newpath =  '/opt/tomcat/webapps/licit/assets/' + filename;
-
-          log(filename);
+          const newpath = path.join(
+            process.cwd(),
+            path.sep,
+            'images',
+            path.sep,
+            filename
+          );
+          log(oldpath);
+          log(newpath);
           mv(oldpath, newpath, function (err) {
             if (err) {
-              response.data.json({ 'error': err });
+              response.writeHead(500, {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Origin': '*',
+              });
+              response.end(
+                JSON.stringify({
+                  error: err,
+                })
+              );
+              return;
             } else {
-                            respdata = {
+              const imgSrc = proto + '://' + host + '/assets/' + filename;
+              log(imgSrc);
+              const respdata = {
                 id: fileid,
-                width: 0,
-                height: 0,
-                src: '/assets/' + filename,
+                width: 10,
+                height: 10,
+                src: imgSrc,
               };
+              response.writeHead(200, {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Origin': '*',
+              });
+              response.end(JSON.stringify(respdata));
+              return;
             }
-            response.end(JSON.stringify(respdata));
           });
         });
       }
 
-      request.on('end', function () {
-          // assume data is posted as `application/json`.
-          request.params = JSON.parse(String(body));
-          // log(body);
-          log(request.params);
-          handleServerRequest(this, request, response);
-          body = null;
-
-      }.bind(this));
+      let body = '';
+      if (!skip) {
+        request.on('data', function (chunk) {
+          log('data');
+          if (!skip) {
+            body += chunk.toString();
+          }
+        });
+        request.on(
+          'end',
+          function () {
+            log('end');
+            if (!skip) {
+              // assume data is posted as `application/json`.
+              request.params = JSON.parse(String(body));
+              // log(body);
+              //log(request.params);
+              handleServerRequest(this, request, response);
+            }
+            body = null;
+          }.bind(this)
+        );
+      }
     } else {
-      request.params = normalizeParams(parsed.query);
-      handleServerRequest(this, request, response);
+      const se = /^\/assets/;
+      if (se.test(reqPath)) {
+        const str = request.url;
+        const filename = str.substring(str.lastIndexOf('/') + 1);
+        const newpath = path.join(
+          process.cwd(),
+          path.sep,
+          'images',
+          path.sep,
+          filename
+        );
+        // serve static
+        fs.readFile(newpath, function (err, data) {
+          if (err) {
+            response.writeHead(404);
+            response.end(JSON.stringify(err));
+            return;
+          }
+          response.writeHead(200);
+          response.end(data);
+        });
+      } else {
+        request.params = normalizeParams(parsed.query);
+        handleServerRequest(this, request, response);
+      }
     }
-  }
-};
+  };
+}
 
 function handleServerRequest(server, request, response) {
   try {
@@ -126,28 +184,28 @@ function handleServerRequest(server, request, response) {
     }
 
     action = request.method.toLowerCase() + '_' + String(action);
-    log(action);
+    //log(action);
     const responseData = getResponseData(
       server.controller,
       action,
       params,
       request,
-      response,
+      response
     );
     if (!responseData) {
       throw new Error('responseData not found for ' + action);
     }
 
     if (responseData instanceof Promise) {
-      responseData.
-        then((data) => {
+      responseData
+        .then((data) => {
           response.writeHead(200, {
             'Content-Type': 'text/plain',
             'Access-Control-Allow-Origin': '*',
           });
           response.end(JSON.stringify(data, null, 2));
-        }).
-        catch(error => {
+        })
+        .catch((error) => {
           response.writeHead(error.status || 500, {
             'Content-Type': 'text/plain',
             'Access-Control-Allow-Origin': '*',
@@ -164,13 +222,15 @@ function handleServerRequest(server, request, response) {
     response.end(JSON.stringify(responseData, null, 2));
   } catch (error) {
     console.log(error);
-    const params  = request.params || null;
+    const params = request.params || null;
     response.writeHead(500, {
       'Content-Type': 'text/plain',
       'Access-Control-Allow-Origin': '*',
     });
-    const message = (error.message || 'Unknown Error') +
-      '\n\nparams\n\n' + JSON.stringify(params, null, 2);
+    const message =
+      (error.message || 'Unknown Error') +
+      '\n\nparams\n\n' +
+      JSON.stringify(params, null, 2);
     response.end(message);
   }
 }
@@ -187,14 +247,16 @@ function getResponseData(
   const fn = controller[method];
   if (typeof fn === 'function') {
     const result = fn.call(controller, params, request, response);
-    return Object.assign(result, {__action__: action});
+    return Object.assign(result, {
+      __action__: action,
+    });
   }
   throw new Error('method ' + method + ' is unsupported');
 }
 
 function normalizeParams(params: Object) {
   const re = /\d+/;
-  Object.keys(params).forEach(key => {
+  Object.keys(params).forEach((key) => {
     const value = params[key];
     if (typeof value === 'string' && re.test(value)) {
       params[key] = parseInt(value, 10);
@@ -205,7 +267,7 @@ function normalizeParams(params: Object) {
 
 function log(...args: any): void {
   console.log('==========================================================\n');
-  console.log(args.map(a => JSON.stringify(a)).join(', '));
+  console.log(args.map((a) => JSON.stringify(a)).join(', '));
   console.log('----------------------------------------------------------\n');
 }
 
